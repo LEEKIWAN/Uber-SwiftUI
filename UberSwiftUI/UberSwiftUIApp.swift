fa1da4f2b046eb6dafce9a31ebdd20a064a027e9
이기완 <kiwan.lee@waiker.ai>
2023-01-31 17:11:45 +0900

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by 이기완 on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
