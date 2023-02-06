2c6390e0b9f329f106729674108523c499e3833b
이기완 <kiwan.lee@waiker.ai>
2023-02-06 16:19:36 +0900

    add: Generating route to Destination

--- UberSwiftUI/Core/Home/Views/MapViewRepresentable.swift
+++ UberSwiftUI/Core/Home/Views/MapViewRepresentable.swift
@@ -14,6 +14,7 @@ struct MapViewRepresentable: UIViewRepresentable {
     let mapView: MKMapView = MKMapView()
 //    let locationManager = LocationManager()
     
+    
     @EnvironmentObject var locationViewModel: LocationSearchViewModel
     
     func makeUIView(context: Context) -> MKMapView {
@@ -29,6 +30,8 @@ struct MapViewRepresentable: UIViewRepresentable {
         if let coordinate = locationViewModel.selectedLocationCoordinate {
             print("DEBUG: Selected Location in map view \(coordinate)")
             
+            context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
+            context.coordinator.configurePolyline(withDestinationCoordinate: coordinate)
         }
     }
 
@@ -38,22 +41,74 @@ struct MapViewRepresentable: UIViewRepresentable {
 }
 
 extension MapViewRepresentable {
+    
     class Coordinator: NSObject, MKMapViewDelegate {
         let parent: MapViewRepresentable
+        
+        var userLocationCoordinate: CLLocationCoordinate2D?
+        
+        // MARK: - Life Cycle
         
         init(parent: MapViewRepresentable) {
             self.parent = parent
             super.init()
         }
         
+        // MARK: - MKMapViewDelegate
         
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            self.userLocationCoordinate = userLocation.coordinate
             let region = MKCoordinateRegion(center: .init(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude),
                                             span: .init(latitudeDelta: 0.05, longitudeDelta: 0.05))
             
-//            print(userLocation.location)
-            
             parent.mapView.setRegion(region, animated: true)
+        }
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let polyline = MKPolylineRenderer(overlay: overlay)
+            polyline.strokeColor = .systemBlue
+            polyline.lineWidth = 6
+            return polyline
+        }
+        
+        // MARK: - Helpers
+        
+        func addAndSelectAnnotation(withCoordinate coordinate: CLLocationCoordinate2D) {
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            parent.mapView.addAnnotation(annotation)
+            parent.mapView.selectAnnotation(annotation, animated: true)
+            
+            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true )
+        }
+        
+        func configurePolyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D) {
+            guard let userLocationCoordinate = userLocationCoordinate else { return }
+            
+            getDestinationRoute(from: userLocationCoordinate, to: coordinate) { route in
+                self.parent.mapView.addOverlay(route.polyline)
+            }
+        }
+        
+        func getDestinationRoute(from userLocation: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D, completion: @escaping (MKRoute) -> Void) {
+            let userPlacemark = MKPlacemark(coordinate: userLocation)
+            let destPlacemark = MKPlacemark(coordinate: destination)
+            
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: userPlacemark)
+            request.destination = MKMapItem(placemark: destPlacemark)
+            let directions = MKDirections(request: request)
+            
+            directions.calculate { response, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+                
+                guard let route = response?.routes.first else { return }
+                completion(route)
+            }
         }
     }
 }
